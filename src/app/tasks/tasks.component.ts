79298f013def4ed63b60e3db8d49c156eef44d02
tahsinturag <tahsinturag10@gmail.com>
2024-11-24 16:37:56 +0600

    alternative way of Extracting Query Parameters via Observables 284

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,8 @@
-import {Component, computed, inject, input} from '@angular/core';
+import {Component, computed, DestroyRef, inject, input, OnInit} from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import {TasksService} from "./tasks.service";
-import {RouterLink} from "@angular/router";
+import {ActivatedRoute, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-tasks',
@@ -11,15 +11,26 @@ import {RouterLink} from "@angular/router";
   styleUrl: './tasks.component.css',
   imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   userId = input.required<string>();
-  order = input<'asc'|'desc'>();
+  // order = input<'asc'|'desc'>();
   // 283 automatic input binding
+
+  order?:'asc'|'desc';
+  // 284
   private tasksService = inject(TasksService);
-  // 276
   userTasks = computed(() =>
     this.tasksService.allTasks().filter((task) => task.userId === this.userId())
 
   );
-// 275
+
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+  ngOnInit(): void {
+    const subscription = this.activatedRoute.queryParams.subscribe({
+      next: (params) =>( this.order = params['order']),
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
