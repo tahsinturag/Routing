f7b74d143f0321db08c5ed0a32aa8240a6945489
tahsinturag <tahsinturag10@gmail.com>
2024-11-24 13:05:36 +0600

    alternative Dynamic Route Parameters via Observables 272

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,32 @@ import {Routes} from "@angular/router";
 import {TasksComponent} from "./tasks/tasks.component";
 import {NoTaskComponent} from "./tasks/no-task/no-task.component";
 import {UserTasksComponent} from "./users/user-tasks/user-tasks.component";
+import {TaskComponent} from "./tasks/task/task.component";
+import {NewTaskComponent} from "./tasks/new-task/new-task.component";
 
 export const routes: Routes = [
   {
     path: '',
     component: NoTaskComponent,
   },
-  {
-    path: 'tasks',
-    component: TasksComponent,
-  },
+  // {
+  //   path: 'tasks',
+  //   component: TasksComponent,
+  // },
   {
     path: 'users/:userId',
     component: UserTasksComponent,
+    children: [
+      {
+        path: 'tasks',
+        component: TasksComponent,
+      }
+    ]
+  },
+  {
+    // 273
+    path: 'tasks/new',
+    component: NewTaskComponent
   }
 ];
 //2
--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,18 +1,42 @@
-import {Component, computed, inject, input} from '@angular/core';
+import {Component, computed, DestroyRef, inject, input, OnInit} from '@angular/core';
 import {UsersService} from "../users.service";
+import {ActivatedRoute, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [
+    RouterOutlet
+  ]
 })
-export class UserTasksComponent {
+export class UserTasksComponent implements OnInit {
+  userName = '';
   userId = input.required<string>();
   private usersService = inject(UsersService);
-  userName = computed(
-    () => this.usersService.users.find((u) => u.id === this.userId())?.name
-  );
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+  // alternative way dynamic routing via observables
+  // userName = computed(
+  //   () => this.usersService.users.find((u) => u.id === this.userId())?.name
+  // );
+
+  ngOnInit() {
+    console.log(this.activatedRoute);
+    const subscription = this.activatedRoute.paramMap.subscribe({
+      next: (paramMap) => {
+        this.userName=
+          this.usersService.users.find((u) => u.id === paramMap.get('userId'))
+            ?.name || ''
+      },
+    });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
+
+// 272
 //new 5-270
 //input import korte hbe
+
+
